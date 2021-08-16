314db7ec0ead879b3c0f4c94807dbbd7314a629f
rambertm <58461570+rambertm@users.noreply.github.com>
2021-08-16 21:30:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const CONTENTS = [
   {url : "https://youtu.be/mf3efASc-F0"}
 ]
 
-document.getElementById('mainDIV').innerHTML = "<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/mf3efASc-F0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"
+document.getElementById('mainDIV').innerHTML = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/mf3efASc-F0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
