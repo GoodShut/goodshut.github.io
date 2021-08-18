498b4dc7df3c55cbf6100238c1503b609bf7806d
rambertm <58461570+rambertm@users.noreply.github.com>
2021-08-19 00:03:29 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+const CONTENTS = [
+  {videoId: 'mf3efASc-F0'
+  ,tags: ['TH14', 'CL', 'DG', 'DR']},
+  {videoId: '7hCfXjRTA58'
+  ,tags: ['TH12', 'QH', 'LL']},
+  {videoId: 'LuhzYk2GSX0'
+  ,tags: ['TH12', 'QH', 'LL']},
+  {videoId: 'liSeQta5GYU'
+  ,tags: ['TH14', 'QH', 'DR']}
+];
