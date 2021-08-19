547ba254f617c1e0859315ad25a6c533e56edcf4
rambertm <58461570+rambertm@users.noreply.github.com>
2021-08-19 22:39:57 +0900

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const CONTENTS = [
   {videoId: 'mf3efASc-F0'
   ,tags: ['TH_14', 'LO_IL', 'ST_CL', 'ST_DG', 'ST_DR']},
   {videoId: '7hCfXjRTA58'
-  ,tags: ['TH_12', 'LO_AS', 'ST_QH', 'ST_LL']},
+  ,tags: ['TH_12', 'LO_IL', 'ST_QH', 'ST_LL']},
   {videoId: 'LuhzYk2GSX0'
   ,tags: ['TH_12', 'LO_IL', 'ST_QH', 'ST_LL']},
   {videoId: 'liSeQta5GYU'
