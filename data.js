f441f74b6a9884c4b2dcd858ab57c72aeb144112
rambertm <58461570+rambertm@users.noreply.github.com>
2021-08-20 11:33:01 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
 const CONTENTS = [
+  {videoId: 'unc-kaFfv9Q'
+  ,tags: ['TH_13', 'LO_PZ', 'ST_QH', 'ST_DR']},
   {videoId: 'mf3efASc-F0'
   ,tags: ['TH_14', 'LO_IL', 'ST_CL', 'ST_DG', 'ST_DR']},
   {videoId: '7hCfXjRTA58'
@@ -6,7 +8,5 @@ const CONTENTS = [
   {videoId: 'LuhzYk2GSX0'
   ,tags: ['TH_12', 'LO_IL', 'ST_QH', 'ST_LL']},
   {videoId: 'liSeQta5GYU'
-  ,tags: ['TH_14', 'LO_SQ', 'ST_QH', 'ST_DR']},
-  {videoId: 'unc-kaFfv9Q'
-  ,tags: ['TH_13', 'LO_PZ', 'ST_QH', 'ST_DR']}
+  ,tags: ['TH_14', 'LO_SQ', 'ST_QH', 'ST_DR']}
 ];
