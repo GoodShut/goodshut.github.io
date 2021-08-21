a1fb57ac3e284519afd6b250cbbfbf63160f0b73
rambertm <58461570+rambertm@users.noreply.github.com>
2021-08-21 10:31:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,42 +15,30 @@ class Swipe {
         }.bind(this), false);
 
     }
-
     onLeft(callback) {
         this.onLeft = callback;
-
         return this;
     }
-
     onRight(callback) {
         this.onRight = callback;
-
         return this;
     }
-
     onUp(callback) {
         this.onUp = callback;
-
         return this;
     }
-
     onDown(callback) {
         this.onDown = callback;
-
         return this;
     }
-
     handleTouchMove(evt) {
         if ( ! this.xDown || ! this.yDown ) {
             return;
         }
-
         var xUp = evt.touches[0].clientX;
         var yUp = evt.touches[0].clientY;
-
         this.xDiff = this.xDown - xUp;
         this.yDiff = this.yDown - yUp;
-
         if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) { // Most significant.
             if ( this.xDiff > 0 ) {
                 this.onLeft();
@@ -64,12 +52,10 @@ class Swipe {
                 this.onDown();
             }
         }
-
         // Reset values.
         this.xDown = null;
         this.yDown = null;
     }
-
     run() {
         this.element.addEventListener('touchmove', function(evt) {
             this.handleTouchMove(evt).bind(this);
