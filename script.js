72b1ce5ee4bf4cdbbbd9bea00bbe616179b4e63c
rambertm <58461570+rambertm@users.noreply.github.com>
2021-08-21 11:00:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,86 +3,6 @@ const PAGENATION = document.querySelector('.pagenation');
 const FILTER = {tags :[], passed: [], currentpage: 1};
 const INITIALLOAD = 4;
 
-class Swipe {
-    constructor(element) {
-        this.xDown = null;
-        this.yDown = null;
-        this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
-
-        this.element.addEventListener('touchstart', function(evt) {
-            this.xDown = evt.touches[0].clientX;
-            this.yDown = evt.touches[0].clientY;
-        }, false);
-
-    }
-    onLeft(callback) {
-        this.onLeft = callback;
-        return this;
-    }
-    onRight(callback) {
-        this.onRight = callback;
-        return this;
-    }
-    onUp(callback) {
-        this.onUp = callback;
-        return this;
-    }
-    onDown(callback) {
-        this.onDown = callback;
-        return this;
-    }
-    handleTouchMove(evt) {
-        if ( ! this.xDown || ! this.yDown ) {
-            return;
-        }
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-        this.xDiff = this.xDown - xUp;
-        this.yDiff = this.yDown - yUp;
-        if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) { // Most significant.
-            if ( this.xDiff > 0 ) {
-                this.onLeft();
-            } else {
-                this.onRight();
-            }
-        } else {
-            if ( this.yDiff > 0 ) {
-                this.onUp();
-            } else {
-                this.onDown();
-            }
-        }
-        // Reset values.
-        this.xDown = null;
-        this.yDown = null;
-    }
-    run() {
-        this.element.addEventListener('touchmove', function(evt) {
-            this.handleTouchMove(evt);
-        }, false);
-    }
-}
-const swiper = new Swipe(document.body);
-swiper.onRight(function(){
-  alert('onRight');
-  let currentPageButton = document.querySelector('.pagenation__indexbutton--selected');
-  let prevPageButton = currentPageButton.previousElementSibling;
-  if (prevPageButton){
-    prevPageButton.click();
-  }
-  return
-});
-swiper.onLeft(function(){
-  alert('onLeft');
-  let currentPageButton = document.querySelector('.pagenation__indexbutton--selected');
-  let nextPageButton = currentPageButton.nextElementSibling;
-  if (nextPageButton){
-    nextPageButton.click();
-  }
-  return
-});
-swiper.run();
-
 function SetPageNation(startPage){
   ClearChildren(PAGENATION);
   let count = FILTER.passed.length;
@@ -264,7 +184,57 @@ function Main(){
 }
 Main();
 
+///// TOUCH, SWIPE EVENT givanse @ stackoverflow /////
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+var xDown = null;
+var yDown = null;
+function getTouches(evt) {
+  return evt.touches
+}
 
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+          let currentPageButton = document.querySelector('.pagenation__indexbutton--selected');
+          let nextPageButton = currentPageButton.nextElementSibling;
+          if (nextPageButton){
+            nextPageButton.click();
+          }
+        } else {
+          let currentPageButton = document.querySelector('.pagenation__indexbutton--selected');
+          let prevPageButton = currentPageButton.previousElementSibling;
+          if (prevPageButton){
+            prevPageButton.click();
+          }
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */
+        } else {
+            /* up swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
 /////////////////////////Functions///////////////////////////////
 function ClearChildren(element){
   while(element.firstChild){
